71ffa3301135aebd9d6e009486686ba20ce88e32
Pier-Lionel Sgard <hello@plsgd.com>
2018-03-21 15:36:14 +0100

    close nav before each route change

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,7 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
+  store.dispatch('setDrawer', false)
   if (to.matched.some(record => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
